42499523c0a079ed6f5ac16375aae1fa2f06e44d
Muhammad Ateeb <114637585+muhammadateeb86@users.noreply.github.com>
2024-08-05 01:13:54 -0700

    Update Template1.jsx

--- src/Components/Template1.jsx
+++ src/Components/Template1.jsx
@@ -1,23 +1,88 @@
 import React, { useState } from 'react'
-import template1Css from "../Css/template1.module.css"
+
+const styles = {
+    emailContainer: {
+      height: 'fit-content',
+      width: '400px',
+      margin: '0 auto',
+      backgroundColor: '#ffffff',
+      boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+    },
+    bannerHeader: {
+      backgroundColor: '#333333',
+      color: '#ffffff',
+      padding: '20px',
+      textAlign: 'center',
+    },
+    bannerHeaderH1: {
+      margin: '0',
+      fontSize: '24px',
+    },
+    bannerMain: {
+      padding: '20px',
+    },
+    bannerMainImg: {
+      width: '100%',
+      height: '400px',
+      objectFit: 'cover',
+      borderRadius: '8px 8px 0 0',
+    },
+    bannerMainH2: {
+      color: '#333333',
+      fontSize: '20px',
+    },
+    bannerMainP: {
+      color: '#666666',
+      fontSize: '16px',
+    },
+    button: {
+      display: 'block',
+      width: '200px',
+      margin: '20px auto',
+      padding: '10px',
+      backgroundColor: '#ff6347',
+      color: '#ffffff',
+      textAlign: 'center',
+      textDecoration: 'none',
+      borderRadius: '5px',
+      fontSize: '18px',
+    },
+    footer: {
+      backgroundColor: '#333333',
+      color: 'hsl(0, 0%, 100%)',
+      padding: '20px',
+      textAlign: 'center',
+    },
+    footerA: {
+      color: '#ff6347',
+      textDecoration: 'none',
+      margin: '0 10px',
+    },
+    banner1HeroImage: {
+      width: '100%',
+      height: '400px',
+      objectFit: 'cover',
+      borderRadius: '8px 8px 0 0',
+    },
+  };
 
 const Template1 = ({ result }) => {
 
     const [url, setUrl] = useState(null);
 
   return (
-    <div className={template1Css['email-container']}>
-        <div className={template1Css["banner_header"]}>
+    <div style={styles.emailContainer}>
+        <div style={styles.bannerHeader}>
             <h1>{result.subject}</h1>
         </div>
-        <div className={template1Css.banner_main}>
-            <img className={template1Css["banner1-hero-image"]} src={result.image_url} alt="Generated" />
+        <div style={styles.bannerMain}>
+            <img style={styles.banner1HeroImage} src={result.image_url} alt="Generated" />
             <h2>{result.promo}</h2>
             <p>{result.description}</p>
-            <a href={result.url} className={template1Css.button}>Shop Now</a>
+            <a href={result.url} style={styles.button}>Shop Now</a>
         </div>
-        <div className={template1Css.footer}>
-            <a href="#">Unsubscribe</a>
+        <div style={styles.footer}>
+            <a href="#" style={styles.footerA}>Unsubscribe</a>
         </div>
     </div>
   )
